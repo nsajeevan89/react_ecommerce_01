f29bc57da043c18165386f38ec68d44940ab6e68
sajeevann <nsajeevan89@gmail.com>
2023-10-04 11:52:01 +0530

    Signed-off-by: sajeevann <nsajeevan89@gmail.com>Complete

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../Product";
+import { ShopContext } from "../../Context/ShopContext";
+import CartItem from "./CartItem";
+import "./Cart.css";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
+  const { cartItems, getTotalCartAmout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmout();
+  const navigate = useNavigate();
   return (
-    <div className="shop">
+    <div className="cart">
       <div>
-        <h1>E-Commerce 01</h1>
+        <h1>Your Cart Items</h1>
       </div>
-    </div>
-  )
-}
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
 
-export default Cart
+      {totalAmount > 0 ? (
+        <div className="checkOut">
+          <p>Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Check Out</button>
+        </div>
+      ) : (
+        <h1>Your Cart is Empty!</h1>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
--- src/Pages/Shop/Product.jsx
+++ src/Pages/Shop/Product.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Product = (props) => {
-    const {id, productName, price, productImage} = props.data;
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const cartItemAmout=cartItems[id];
   return (
     <div className="product">
-      <img src={productImage}/>
+      <img src={productImage} />
       <div className="description">
-        <p><b>{productName}</b></p>
+        <p>
+          <b>{productName}</b>
+        </p>
         <p>{price}</p>
       </div>
-      <button className="addToCartBttn">Add To Cart</button>
+      <button className="addToCartBttn" onClick={() => addToCart(id)} key={id}>
+        Add To Cart {cartItemAmout > 0 && <>({cartItemAmout})</>}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
